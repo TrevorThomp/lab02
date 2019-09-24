98fe5b222c97164b8abe0da3830c0232c850a705
tskyles <travis.skyles@gmail.com>
2019-09-24 14:58:34 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var userName = prompt('Hi, What is your name?')
+alert('Hello, ' + userName + '! Please answer the following questions using yes or no.');
+
+var questionOne = prompt('Was I born January?');
+
+if(questionOne.toLowerCase() === 'yes') {
+    //console.log('You are correct!')
+    alert('You are correct!')
+} else if(questionOne.toLowerCase() === 'no') {
+    //console.log('Sorry that is incorrect..');
+    alert('Sorry that is incorrect');
+} 
+
+var questionTwo = prompt('Do I like cats more than dogs?');
+
+if(questionTwo.toLowerCase() === 'no') {
+    //console.log('You are correct!')
+    alert('You are correct!')
+} else if(questionTwo.toLowerCase() === 'yes') {
+    //console.log('Sorry that is incorrect..');
+    alert('Sorry that is incorrect');
+}
+
+var questionThree = prompt('Am I from California?');
+
+if(questionThree.toLowerCase() === 'no') {
+    //console.log('You are correct!')
+    alert('You are correct!')
+} else if(questionThree.toLowerCase() === 'yes') {
+    //console.log('Sorry that is incorrect..');
+    alert('Sorry that is incorrect');
+}
+
+var questionFour = prompt('Is my favorite sport rugby?');
+
+if(questionFour.toLowerCase() === 'yes') {
+    //console.log('You are correct!')
+    alert('You are correct!')
+} else if(questionFour.toLowerCase() === 'no') {
+   // console.log('Sorry that is incorrect..');
+   alert('Sorry that is incorrect');
+
+}
+
+ var questionFive = prompt('Do I have blue eyes?');
+ 
+ if(questionFour.toLowerCase() === 'yes') {
+    //console.log('You are correct!')
+    alert('You are correct!')
+} else if(questionFour.toLowerCase() === 'no') {
+    //console.log('Sorry that is incorrect..');
+    alert('Sorry that is incorrect');
+}
+
+alert('Well done ' + userName + '! Thanks for visiting and taking my quiz!');
