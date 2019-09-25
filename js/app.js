dc63b61f0268f08dd89b8a4a476b32329c5656f2
tskyles <travis.skyles@gmail.com>
2019-09-25 12:36:55 -0700

    guess game complete

--- js/app.js
+++ js/app.js
@@ -1,57 +1,87 @@
 'use strict';
 
-var userName = prompt('Hi, What is your name?')
-alert('Hello, ' + userName + '! Please answer the following questions using yes or no.');
+// var userName = prompt('Hi, What is your name?')
+// alert('Hello, ' + userName + '! Please answer the following questions using yes or no.');
 
-var questionOne = prompt('Was I born January?');
+// var questionOne = prompt('Was I born January?');
 
-if(questionOne.toLowerCase() === 'yes') {
-    //console.log('You are correct!')
-    alert('You are correct!');
-} else if(questionOne.toLowerCase() === 'no') {
-    //console.log('Sorry that is incorrect..');
-    alert('Sorry that is incorrect');
-} 
+// if(questionOne.toLowerCase() === 'yes') {
+//     //console.log('You are correct!')
+//     alert('You are correct!');
+// } else if(questionOne.toLowerCase() === 'no') {
+//     //console.log('Sorry that is incorrect..');
+//     alert('Sorry that is incorrect');
+// } 
 
-var questionTwo = prompt('Do I like cats more than dogs?');
+// var questionTwo = prompt('Do I like cats more than dogs?');
 
-if(questionTwo.toLowerCase() === 'no') {
-    //console.log('You are correct!')
-    alert('You are correct!');
-} else if(questionTwo.toLowerCase() === 'yes') {
-    //console.log('Sorry that is incorrect..');
-    alert('Sorry that is incorrect');
-}
+// if(questionTwo.toLowerCase() === 'no') {
+//     //console.log('You are correct!')
+//     alert('You are correct!');
+// } else if(questionTwo.toLowerCase() === 'yes') {
+//     //console.log('Sorry that is incorrect..');
+//     alert('Sorry that is incorrect');
+// }
 
-var questionThree = prompt('Am I from California?');
+// var questionThree = prompt('Am I from California?');
 
-if(questionThree.toLowerCase() === 'no') {
-    //console.log('You are correct!')
-    alert('You are correct!');
-} else if(questionThree.toLowerCase() === 'yes') {
-    //console.log('Sorry that is incorrect..');
-    alert('Sorry that is incorrect');
-}
+// if(questionThree.toLowerCase() === 'no') {
+//     //console.log('You are correct!')
+//     alert('You are correct!');
+// } else if(questionThree.toLowerCase() === 'yes') {
+//     //console.log('Sorry that is incorrect..');
+//     alert('Sorry that is incorrect');
+// }
 
-var questionFour = prompt('Is my favorite sport rugby?');
+// var questionFour = prompt('Is my favorite sport rugby?');
 
-if(questionFour.toLowerCase() === 'yes') {
-    //console.log('You are correct!')
-    alert('You are correct!');
-} else if(questionFour.toLowerCase() === 'no') {
-   // console.log('Sorry that is incorrect..');
-   alert('Sorry that is incorrect');
+// if(questionFour.toLowerCase() === 'yes') {
+//     //console.log('You are correct!')
+//     alert('You are correct!');
+// } else if(questionFour.toLowerCase() === 'no') {
+//    // console.log('Sorry that is incorrect..');
+//    alert('Sorry that is incorrect');
 
-}
+// }
 
- var questionFive = prompt('Do I have blue eyes?');
+//  var questionFive = prompt('Do I have blue eyes?');
  
- if(questionFour.toLowerCase() === 'yes') {
-    //console.log('You are correct!')
-    alert('You are correct!');
-} else if(questionFour.toLowerCase() === 'no') {
-    //console.log('Sorry that is incorrect..');
-    alert('Sorry that is incorrect');
-}
+//  if(questionFour.toLowerCase() === 'yes') {
+//     //console.log('You are correct!')
+//     alert('You are correct!');
+// } else if(questionFour.toLowerCase() === 'no') {
+//     //console.log('Sorry that is incorrect..');
+//     alert('Sorry that is incorrect');
+// }
 
-alert('Well done ' + userName + '! Thanks for visiting and taking my quiz!');
+
+var correctNumber = 7;
+var guessAttempts = 0;
+
+do {
+    var questionSix = prompt('Guess a number between 1 and 10');
+    if(parseInt(questionSix) === correctNumber) {
+        console.log('you are correct!');
+        alert('You are correct!');
+        guessAttempts = 4;
+    } else if(parseInt(questionSix) > 7) {
+        guessAttempts++;
+        console.log('Too High' + guessAttempts);
+    } else {
+        guessAttempts++;
+        console.log('Too Low' + guessAttempts);
+    }
+}while (guessAttempts < 4);
+
+
+// if(questionSix === 7) {
+//     console.log('you are correct!');
+//     alert('You are correct!');
+// } else if(questionSix >7) {
+//     console.log('Too High');
+// } else {
+//     console.log('Too Low');
+// }
+
+
+// alert('Well done ' + userName + '! Thanks for visiting and taking my quiz!');
